0095d31643585ae217c13d3ab15fd777cd7c837e
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-04-20 16:38:16 -0700

    Add API Docs v1.3.1

--- electric.config.js
+++ electric.config.js
@@ -4,6 +4,22 @@ var marble = require('marble');
 var path = require('path');
 
 module.exports = {
+	apiConfig: {
+		layout: 'api',
+		project: {
+			refs: ['v1.3.1'],
+			repo: 'alloy-editor',
+			src: [
+				'src/core/**/*.js',
+				'src/plugins/**/*.js',
+				'src/ui/react/src/**/*.js'
+			],
+			user: 'liferay'
+		}
+	},
+	entryPoints: {
+		electricAPI: path.join(__dirname, 'src/partials/ElectricAPI.js')
+	},
 	metalComponents: ['electric-marble-components'],
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
